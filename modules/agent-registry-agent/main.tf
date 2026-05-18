344386359d8dbd83259375bb67250ee31234171c
Daisy <daisypkhoja@google.com>
2026-05-18 12:22:42 +0000

    change

--- modules/agent-registry-agent/main.tf
+++ modules/agent-registry-agent/main.tf
@@ -16,7 +16,7 @@
 
 resource "time_sleep" "wait_for_auto_registration_for_agent_in_registry" {
   create_duration = var.wait_time
-  depends_on = [var.module_depends_on]
+  depends_on      = [var.module_depends_on]
 }
 
 data "google_agent_registry_agent" "default" {
